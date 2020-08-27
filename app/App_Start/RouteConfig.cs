6ce5ce26d142ef558727d36ba85f5516f9abdc49
phildkim <philipdanielkim@gmail.com>
2020-08-26 18:00:57 -0700

    Update

--- app/App_Start/RouteConfig.cs
+++ app/App_Start/RouteConfig.cs
@@ -7,13 +7,11 @@ namespace app
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
             routes.MapRoute(
                 name: "Trader",
                 url: "{controller}/{action}/{id}",
